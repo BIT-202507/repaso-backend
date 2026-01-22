8cf5c89ed6092473613e510c0a3ba8c88a97176c
Juan Carlos Jiménez Gutiérrez <jangojes.dev@gmail.com>
2026-01-21 20:52:41 -0500

    [+] npm i express [+] index.js: Lanzan un servidor usando Express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('Bienvenidos a mi API Rest Full');
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+// Lanzar servidor
+app.listen( PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
